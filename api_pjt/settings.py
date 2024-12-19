0948aa0c555cdabfd56e5cffeab27b7ea02323fb
yejingksdpwls <yejingksdpwls@gmail.com>
2024-12-19 22:55:55 +0900

    add : 정적 파일 경로 추가

--- api_pjt/settings.py
+++ api_pjt/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     'silk.middleware.SilkyMiddleware',
 ]
 
+
 ROOT_URLCONF = "api_pjt.urls"
 
 TEMPLATES = [
@@ -160,6 +161,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
